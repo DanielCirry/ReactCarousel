2510f9ab7fae3f57678fda7861ffd5f0fadd40ce
daniele cirrincione <rudi1cd@gmail.com>
2020-04-17 00:54:39 +0100

    modifies Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,37 +6,32 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
-import { AutoRotatingCarousel, Slide } from "material-auto-rotating-carousel";
 
-const Course = props => {
+const Course = (props) => {
   return (
-    <>
-      {props.course ? (
-        <AutoRotatingCarousel>
-          <Card>
-            <CardContent>
-              <CardMedia
-                image={props.course.fields.courseImage}
-                title={props.course.fields.title}
-                style={{ height: "100%", paddingTop: "56.25%" }}
-              />
-              <Typography variant="h5">{props.course.fields.title}</Typography>
-              <Typography>{props.course.fields.description}</Typography>
-              <CardActions>
-                <Button
-                  size="small"
-                  color="primary"
-                  href={props.course.fields.url}
-                  target="_blank"
-                >
-                  Go To COurse
-                </Button>
-              </CardActions>
-            </CardContent>
-          </Card>
-        </AutoRotatingCarousel>
-      ) : null}
-    </>
+    props.course && (
+      <Card>
+        <CardContent>
+          <CardMedia
+            image={props.course.fields.courseImage}
+            title={props.course.fields.title}
+            style={{ height: "100%", paddingTop: "56.25%" }}
+          />
+          <Typography variant="h5">{props.course.fields.title}</Typography>
+          <Typography>{props.course.fields.description}</Typography>
+          <CardActions>
+            <Button
+              size="small"
+              color="primary"
+              href={props.course.fields.url}
+              target="_blank"
+            >
+              Go To COurse
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    )
   );
 };
 
